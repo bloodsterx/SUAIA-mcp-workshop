a3f9d4b205f7fd994e52b3469b6352558ad39397
bloodsterx <rickhonda0@gmail.com>
2025-09-17 17:50:38 +1000

    added resource and portfolio summary tool

--- src/servers/stockbroker-mcp.py
+++ src/servers/stockbroker-mcp.py
@@ -3,18 +3,43 @@ import yfinance as yf
 
 mcp = FastMCP(name="stockbroker")
 
-# provide functionality with tools - similar to POST endpoints; we use tools to execute code/do tasks
-# the llm will scan the tools (indicated by decorator @mcp.tool() and decide on the most appropriate tool for the user's query
-# The llm reads the function signature (arguments and return value), and reads the docstring - these are considered by the llm before deciding on the tool to call and arguments to provide
-
 
 @mcp.tool()
-def get_stock_price(ticker: str) -> float:
+def get_stock_price(ticker: str) -> dict:
     """
     Retrieve the current price of a stock given a ticker symbol
     """
     stock = yf.Ticker(ticker)
     return stock.info["regularMarketPrice"]
+
+# lets give the llm some context - the client is going to look through the exposed resources before deciding which tool to call and what args
+
+
+@mcp.resource("myportfolio://portfolio")
+def get_portfolio() -> dict:
+    """Retrieves and returns stock tickers in the user's portfolio"""
+    return {
+        "portfolio": ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "NVDA", "META", "NFLX", "CSCO", "INTC"]
+    }
+
+
+@mcp.tool()
+def summarize_portfolio(portfolio: list[str]) -> dict:
+    """Summarizes the portfolio of stocks in the user's portfolio"""
+
+    portfolio_summary = {}
+    for ticker in portfolio:
+        stock = yf.Ticker(ticker)
+        summary = {}
+        summary["closing price"] = stock.info["regularMarketPrice"]
+        summary["previous close"] = stock.info["regularMarketPreviousClose"]
+        summary["change"] = stock.info["regularMarketPrice"] - \
+            stock.info["regularMarketPreviousClose"]
+        summary["news"] = stock.news
+
+        portfolio_summary[ticker] = summary
+
+    return portfolio_summary
 
 
 if __name__ == "__main__":
